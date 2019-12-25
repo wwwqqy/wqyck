9927ec085780fdf27992519508d13529557ff8f4
wwwqqy <59213540+wwwqqy@users.noreply.github.com>
2019-12-25 11:59:21 +0800

    V1

---
+++ 结课作业/EmailDemo/EmailDemo/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LoadContent;
+
+namespace EmailDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Data myData;
+        public MainWindow()
+        {
+            InitializeComponent();
+            myData = new Data();
+            this.DataContext = myData; 
+        }
+
+        private void bt_loadContent_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 w = new Window1();
+            w.ShowDialog();
+            if(Util.ContentwithLoad!=string.Empty)
+                myData.Content = Util.ContentwithLoad;
+
+        }
+
+        public string FindFileDialog(string filter)
+        {
+            //文件全路径
+            string localFilePath = "";
+            try
+            {
+                Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+                openFileDialog.Filter = filter;// "txt files(*.txt)|*.txt";
+                openFileDialog.RestoreDirectory = true;
+                bool? result = openFileDialog.ShowDialog();
+
+                if (result == true)
+                {
+                    //获得文件路径
+                    return localFilePath = openFileDialog.FileName.ToString();
+                }
+                return localFilePath;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("所选上传文件不正确，请重新选！");
+                return localFilePath;
+            }
+        }
+
+        private void bt_LoadPic_Click(object sender, RoutedEventArgs e)
+        {
+            string path = FindFileDialog("jpg files(*.jpg)|*.jpg|png files(*.png)|*.png|jepg files(*.jepg)|*.jepg");
+            myData.PictureFileName = path;
+        }
+
+        private void bt_OK_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnOK_executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("我执行确定了！");
+        }
+
+        private void OnOK_CanExecuted(object sender, CanExecuteRoutedEventArgs e)
+        {
+            //分别是email地址不空，email地址符合正确的表达式,内容不空,图片地址不空
+            e.CanExecute = !string.IsNullOrEmpty(tb_email.Text) && 
+                CheckEmailAddress(tb_email.Text) && 
+                !string.IsNullOrEmpty(tb_Content.Text) && 
+                !string.IsNullOrEmpty(tb_PicPath.Text);
+        }
+
+        private bool CheckEmailAddress(string address)
+        {
+            string pattern = "^[_a-z0-9-]+(/.[_a-z0-9-]+)*@[a-z0-9-]+(/.[a-z0-9-]+)*$";
+            Regex r = new Regex(pattern);
+            Match m = r.Match(address);
+            if (m.Success)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
